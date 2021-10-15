1656798df5761462ed0b115e3c774b6ab148b163
onigiri3670 <isfhzsojfafioyhkvsulvhlidsl@yahoo.co.jp>
2021-10-15 11:49:22 +0900

    重複録画しないようにする

--- record_live.rb
+++ record_live.rb
@@ -6,6 +6,7 @@ target_channels.each do |cnannel_name, channel_url|
   sleep 1
   lives = fetch_live_info(channel_url)
   lives.each do |live|
+    next if `ps -ax | grep wait_youtubu_live.sh`.include?(live[:url])
     fork do
       `/bin/bash wait_youtubu_live.sh #{live[:url]} > logs/wait_youtube_live_#{live[:id]}.log 2>&1`
     end
