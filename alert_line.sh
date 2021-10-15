391252bd4b33811f72558c4691b0237cfb235c03
onigiri3670 <isfhzsojfafioyhkvsulvhlidsl@yahoo.co.jp>
2021-10-15 12:10:59 +0900

    LINE NOTIFY連携による通知を追加

---
+++ alert_line.sh
@@ -0,0 +1,1 @@
+curl -X POST -H 'Authorization: Bearer xMeNR1ojHsOg34U8o71cRSk4vO64zVEJwJ1ZkvwQSHr' -F "message=$1" 'https://notify-api.line.me/api/notify'
--- record_live.rb
+++ record_live.rb
@@ -2,13 +2,18 @@ require 'yaml'
 require_relative 'fetch_live_info'
 
 target_channels = YAML.load(File.read 'target_channel.yml')
-target_channels.each do |cnannel_name, channel_url|
+target_channels.each do |channel_name, channel_url|
   sleep 1
   lives = fetch_live_info(channel_url)
   lives.each do |live|
     next if `ps -ax | grep wait_youtubu_live.sh`.include?(live[:url])
+    `/bin/bash alert_line.sh #{channel_name}のライブが検知されました。 #{live[:url]}`
     fork do
       `/bin/bash wait_youtubu_live.sh #{live[:url]} > logs/wait_youtube_live_#{live[:id]}.log 2>&1`
     end
   end
+rescue => e
+  puts "#{e.message}. #{Time.now}"
+  p e.backtrace
+  `/bin/bash alert_line.sh #{e.message}`
 end
